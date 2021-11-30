4b79d71a3398004fba8b115fac102c8a890b3b8e
Pino Carafa <gcarafa@keyhouse.ie>
2021-11-30 11:12:20 +0000

    Added some comments to make it clearer how this simulator works

--- ShuffleSimulator/MainWindow.xaml.vb
+++ ShuffleSimulator/MainWindow.xaml.vb
@@ -9,6 +9,8 @@ Class MainWindow
 
         mbStopping = False
         mbStopped = False
+
+        'Validate the input. I just check that the user enters valid integers and that the size of the queue doesn't exceed the size of the playlist minus 2
         Dim nLikedSongsSize As Integer = -1
         Try
             nLikedSongsSize = CInt(tbListLength.Text)
@@ -39,10 +41,11 @@ Class MainWindow
             Exit Sub
         End If
 
+        'Create the "Play List". 
         Dim oLikedSongs As Dictionary(Of Integer, SongItem) = New Dictionary(Of Integer, SongItem)
         goRandom = New Random()
 
-        'Populate the "liked songs" list 
+        'Populate the "Play list"
         Dim nCount As Integer = nLikedSongsSize
         While nCount > 0
             Dim oLikedSong As SongItem = New SongItem
@@ -69,8 +72,12 @@ Class MainWindow
         Dim dTotal As Double
         Dim dCount As Double
 
+        'Keep simulating "shuffled play lists" until the user hits Stop
         While True
+            'Next Simulation
             dCount = dCount + 1
+
+            'Copy the Play List into a "running" play list. This ensures that with every Simulation we start with all songs yet "unplayed"
             Dim oCopyList As Dictionary(Of Integer, SongItem)
             oCopyList = New Dictionary(Of Integer, SongItem)
             For Each oSongItem As SongItem In oLikedSongs.Values
@@ -84,6 +91,10 @@ Class MainWindow
             Dim oQueue As List(Of SongItem) = New List(Of SongItem)
             Dim oPlayedSongs As Dictionary(Of Integer, SongItem) = New Dictionary(Of Integer, SongItem)
 
+            'Now we take the first N items out of the play list and move them to the queue.
+            'For example if the Play List contains 1000 "songs" and the Queue size is set to 80,
+            'it will move 80 random "songs" from the (Copied) Play List to the Queue, after which
+            'the queue contains 80 "songs" and the (Copied) Play List contains the other 920 "songs"
             Dim nCount As Integer = nQueueLength
             Dim nMax As Integer = nLikedSongsSize
             While nCount > 0
@@ -95,33 +106,58 @@ Class MainWindow
                 oQueue.Add(oSong)
             End While
 
+            'Now run the simulation
             Dim nSongsPlayedUntilAPreviouslyPlayedSongIsPlayedAgain As Integer = 0
             While True
+                'Count the number of "songs" "played" so far
                 nSongsPlayedUntilAPreviouslyPlayedSongIsPlayedAgain = nSongsPlayedUntilAPreviouslyPlayedSongIsPlayedAgain + 1
+
+                '"Play" the first "song" in the queue
                 Dim oSong As SongItem = oQueue.Item(0)
                 If oSong.Played Then
+                    'If this song has already been played, end the simulation. With the Count we did above we now know how many
+                    '"song"s were "played" in the current Simulation before a "song" that had already been "played" was "played" again
                     Exit While
                 End If
+
+                'Otherwise, flag this "song" as "played" ...
                 oSong.Played = True
+
+                '... remove it from the first position in the Queue ...
                 oQueue.RemoveAt(0)
+
+                '... add it back to the end of the Copied "play list" ...
                 oCopyList.Add(oSong.IndexNumber, oSong)
+
+                '... get a random "song" out of the Copied Play List (remember, this contains the "song"s that are currently not in the queue) ...
                 nMax = oCopyList.Count
                 Dim nSongNumber As Integer = goRandom.Next(minValue:=0, maxValue:=nMax - 1)
                 oSong = oCopyList.Item(oCopyList.Keys(nSongNumber))
+
+                '... remove the "song" from the Copied Play List ...
                 oCopyList.Remove(oSong.IndexNumber)
+
+                '... and add it to the end of the Queue
                 oQueue.Add(oSong)
             End While
 
+            'Add the result of this simulation to the total, and dTotal/dCount will give you the average number of "song"s that were "played"
+            'before the simulation picked up a song that had already been "played" before
             dTotal = dTotal + nSongsPlayedUntilAPreviouslyPlayedSongIsPlayedAgain
             oDispatcher.Invoke(Sub()
+                                   'And update the display with the results
                                    rSimulations.Text = CInt(Math.Round(CDec(dCount), 0)).ToString()
                                    rSongsPlayed.Text = nSongsPlayedUntilAPreviouslyPlayedSongIsPlayedAgain.ToString
                                    rAverage.Text = (dTotal / dCount).ToString()
                                End Sub)
+
+            'If the user hit Stop, end this procedure
             If mbStopping Then
                 mbStopped = True
                 Exit Sub
             End If
+
+            'Otherwise, do the next Simulation with the same parameters.
         End While
 
     End Sub
