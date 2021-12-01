9ab2fb3019d0093d30305ce0755dd3d0e7fe2636
Pino Carafa <gcarafa@keyhouse.ie>
2021-12-01 19:50:21 +0000

    Added a second simulation to show what happens if you re-shuffle the list every time to listen.

--- ShuffleSimulator/MainWindow.xaml.vb
+++ ShuffleSimulator/MainWindow.xaml.vb
@@ -10,34 +10,66 @@ Class MainWindow
         mbStopping = False
         mbStopped = False
 
+        If cbKeepRestarting.IsChecked Then
+            tbNumberUntilRepeat.Visibility = Visibility.Collapsed
+            tbAverageNumberUntilRepeat.Visibility = Visibility.Collapsed
+            tbAverageNumberOfRepeats.Visibility = Visibility.Visible
+        Else
+            tbNumberUntilRepeat.Visibility = Visibility.Visible
+            tbAverageNumberUntilRepeat.Visibility = Visibility.Visible
+            tbAverageNumberOfRepeats.Visibility = Visibility.Collapsed
+        End If
+
         'Validate the input. I just check that the user enters valid integers and that the size of the queue doesn't exceed the size of the playlist minus 2
         Dim nLikedSongsSize As Integer = -1
         Try
-            nLikedSongsSize = CInt(tbListLength.Text)
+            nLikedSongsSize = CInt(txtListLength.Text)
         Catch
 
         End Try
         If nLikedSongsSize < 0 Then
-            MsgBox("enter a valid list length", vbOKOnly Or vbExclamation, "error")
-            tbListLength.Focus()
+            mbStopping = True
+            mbStopped = True
+            MsgBox("enter a valid number for the size of the play list", vbOKOnly Or vbExclamation, "error")
+            txtListLength.Focus()
             Exit Sub
         End If
 
         Dim nQueueLength As Integer = -1
         Try
-            nQueueLength = CInt(tbQueueLength.Text)
+            nQueueLength = CInt(txtQueueLength.Text)
         Catch
 
         End Try
-        If nLikedSongsSize < 0 Then
-            MsgBox("enter a valid queue length", vbOKOnly Or vbExclamation, "error")
-            tbQueueLength.Focus()
+        If nQueueLength < 0 Then
+            mbStopping = True
+            mbStopped = True
+            MsgBox("enter a valid number for the size of the queue", vbOKOnly Or vbExclamation, "error")
+            txtQueueLength.Focus()
             Exit Sub
         End If
 
+        Dim nMaxUntilStop As Integer = -1
+        If cbKeepRestarting.IsChecked Then
+            Try
+                nMaxUntilStop = CInt(txtMaxUntilStop.Text)
+            Catch
+
+            End Try
+            If nMaxUntilStop < 0 Then
+                mbStopping = True
+                mbStopped = True
+                MsgBox("enter a valid maximum number of songs before stopping", vbOKOnly Or vbExclamation, "error")
+                txtMaxUntilStop.Focus()
+                Exit Sub
+            End If
+        End If
+
         If nLikedSongsSize <= nQueueLength + 1 Then
+            mbStopping = True
+            mbStopped = True
             MsgBox("Please enter a Queue that's at least two songs shorter than the length of the list", vbOKOnly Or vbExclamation, "error")
-            tbQueueLength.Focus()
+            txtQueueLength.Focus()
             Exit Sub
         End If
 
@@ -50,14 +82,21 @@ Class MainWindow
         While nCount > 0
             Dim oLikedSong As SongItem = New SongItem
             oLikedSong.IndexNumber = nCount
-            oLikedSong.Played = False
+            oLikedSong.PlayedTimes = 0
             oLikedSongs.Add(nCount, oLikedSong)
             nCount = nCount - 1
         End While
 
-        Dim oThread As System.Threading.Thread = New System.Threading.Thread(Sub()
-                                                                                 ShuffleThem(oLikedSongs:=oLikedSongs, nLikedSongsSize:=nLikedSongsSize, nQueueLength:=nQueueLength, oDispatcher:=Application.Current.Dispatcher)
-                                                                             End Sub)
+        Dim oThread As System.Threading.Thread = Nothing
+        If cbKeepRestarting.IsChecked Then
+            oThread = New System.Threading.Thread(Sub()
+                                                      ShuffleRepeats(oLikedSongs:=oLikedSongs, nLikedSongsSize:=nLikedSongsSize, nQueueLength:=nQueueLength, nMaxUntilStop:=nMaxUntilStop, oDispatcher:=Application.Current.Dispatcher)
+                                                  End Sub)
+        Else
+            oThread = New System.Threading.Thread(Sub()
+                                                      ShuffleThem(oLikedSongs:=oLikedSongs, nLikedSongsSize:=nLikedSongsSize, nQueueLength:=nQueueLength, oDispatcher:=Application.Current.Dispatcher)
+                                                  End Sub)
+        End If
         oThread.SetApartmentState(System.Threading.ApartmentState.STA)
         oThread.Start()
 
@@ -83,7 +122,7 @@ Class MainWindow
             For Each oSongItem As SongItem In oLikedSongs.Values
                 Dim oCopy As SongItem = New SongItem
                 oCopy.IndexNumber = oSongItem.IndexNumber
-                oCopy.Played = False
+                oCopy.PlayedTimes = 0
                 oCopyList.Add(oCopy.IndexNumber, oCopy)
             Next
 
@@ -114,14 +153,14 @@ Class MainWindow
 
                 '"Play" the first "song" in the queue
                 Dim oSong As SongItem = oQueue.Item(0)
-                If oSong.Played Then
+                If oSong.PlayedTimes > 0 Then
                     'If this song has already been played, end the simulation. With the Count we did above we now know how many
                     '"song"s were "played" in the current Simulation before a "song" that had already been "played" was "played" again
                     Exit While
                 End If
 
                 'Otherwise, flag this "song" as "played" ...
-                oSong.Played = True
+                oSong.PlayedTimes = oSong.PlayedTimes + 1
 
                 '... remove it from the first position in the Queue ...
                 oQueue.RemoveAt(0)
@@ -162,6 +201,120 @@ Class MainWindow
 
     End Sub
 
+    Private Sub ShuffleRepeats(ByVal oLikedSongs As Dictionary(Of Integer, SongItem),
+                               ByVal nLikedSongsSize As Integer,
+                               ByVal nQueueLength As Integer,
+                               ByVal nMaxUntilStop As Integer,
+                               ByVal oDispatcher As System.Windows.Threading.Dispatcher)
+
+        Dim nMaxRepeats As Integer
+        Dim dCount As Double = 0
+        Dim dTotal As Double = 0
+        Dim nRunsPerSim As Integer
+
+        While True
+            'Next Simulation
+            dCount = dCount + 1
+            nMaxRepeats = 0
+
+            For Each oSongItem As SongItem In oLikedSongs.Values
+                oSongItem.PlayedTimes = 0
+            Next
+
+            nRunsPerSim = 10
+
+            'Keep simulating "shuffled play lists" until the user hits Stop
+            While nRunsPerSim > 0
+                nRunsPerSim = nRunsPerSim - 1
+
+                'Copy the Play List into a "running" play list. This ensures that with every Simulation we start with all songs yet "unplayed"
+                Dim oCopyList As Dictionary(Of Integer, SongItem)
+                oCopyList = New Dictionary(Of Integer, SongItem)
+                For Each oSongItem As SongItem In oLikedSongs.Values
+                    Dim oCopy As SongItem = New SongItem
+                    oCopy.IndexNumber = oSongItem.IndexNumber
+                    oCopy.PlayedTimes = oSongItem.PlayedTimes
+                    oCopyList.Add(oCopy.IndexNumber, oCopy)
+                Next
+
+                'Create the first Queue of 80 items
+                Dim oQueue As List(Of SongItem) = New List(Of SongItem)
+                Dim oPlayedSongs As Dictionary(Of Integer, SongItem) = New Dictionary(Of Integer, SongItem)
+
+                'Now we take the first N items out of the play list and move them to the queue.
+                'For example if the Play List contains 1000 "songs" and the Queue size is set to 80,
+                'it will move 80 random "songs" from the (Copied) Play List to the Queue, after which
+                'the queue contains 80 "songs" and the (Copied) Play List contains the other 920 "songs"
+                Dim nCount As Integer = nQueueLength
+                Dim nMax As Integer = nLikedSongsSize
+                While nCount > 0
+                    nCount = nCount - 1
+                    nMax = nMax - 1
+                    Dim nSongNumber As Integer = goRandom.Next(minValue:=0, maxValue:=nMax - 1)
+                    Dim oSong As SongItem = oCopyList.Item(oCopyList.Keys(nSongNumber))
+                    oCopyList.Remove(oCopyList.Keys(nSongNumber))
+                    oQueue.Add(oSong)
+                End While
+
+                'Now run the simulation
+                Dim nNumberOfSongsToPlay = goRandom.Next(minValue:=1, maxValue:=nMaxUntilStop)
+                While nNumberOfSongsToPlay > 0
+                    'Count the number of "songs" "played" so far
+                    nNumberOfSongsToPlay = nNumberOfSongsToPlay - 1
+
+                    '"Play" the first "song" in the queue
+                    Dim oSong As SongItem = oQueue.Item(0)
+
+                    'Flag this "song" as "played" ...
+                    oSong.PlayedTimes = oSong.PlayedTimes + 1
+
+                    'Reflect this in the original list of liked songs
+                    oLikedSongs.Item(oSong.IndexNumber).PlayedTimes = oSong.PlayedTimes
+
+                    If oSong.PlayedTimes > nMaxRepeats Then
+                        nMaxRepeats = oSong.PlayedTimes
+                    End If
+
+                    '... remove it from the first position in the Queue ...
+                    oQueue.RemoveAt(0)
+
+                    '... add it back to the end of the Copied "play list" ...
+                    oCopyList.Add(oSong.IndexNumber, oSong)
+
+                    '... get a random "song" out of the Copied Play List (remember, this contains the "song"s that are currently not in the queue) ...
+                    nMax = oCopyList.Count
+                    Dim nSongNumber As Integer = goRandom.Next(minValue:=0, maxValue:=nMax - 1)
+                    oSong = oCopyList.Item(oCopyList.Keys(nSongNumber))
+
+                    '... remove the "song" from the Copied Play List ...
+                    oCopyList.Remove(oSong.IndexNumber)
+
+                    '... and add it to the end of the Queue
+                    oQueue.Add(oSong)
+                End While
+            End While
+
+            dTotal = dTotal + nMaxRepeats
+
+            'Add the result of this simulation to the total, and dTotal/dCount will give you the average number of "song"s that were "played"
+            'before the simulation picked up a song that had already been "played" before
+            oDispatcher.Invoke(Sub()
+                                   'And update the display with the results
+                                   rSimulations.Text = CInt(Math.Round(CDec(dCount), 0)).ToString()
+                                   rAverageRepeat.Text = (dTotal / dCount).ToString()
+                               End Sub)
+
+            'If the user hit Stop, end this procedure
+            If mbStopping Then
+                mbStopped = True
+                Exit Sub
+            End If
+
+            'Otherwise, do the next Simulation with the same parameters.
+        End While
+
+    End Sub
+
     Private Sub btnStop_Click(sender As Object, e As RoutedEventArgs) Handles btnStop.Click
         mbStopping = True
     End Sub
@@ -174,9 +327,19 @@ Class MainWindow
             e.Cancel = True
         End If
     End Sub
+
+    Private Sub cbKeepRestarting_Click(sender As Object, e As RoutedEventArgs) Handles cbKeepRestarting.Click
+        If cbKeepRestarting.IsChecked Then
+            tbMaxUntilStop.Visibility = Visibility.Visible
+            txtMaxUntilStop.Visibility = Visibility.Visible
+        Else
+            tbMaxUntilStop.Visibility = Visibility.Collapsed
+            txtMaxUntilStop.Visibility = Visibility.Collapsed
+        End If
+    End Sub
 End Class
 
 Public Class SongItem
     Public Property IndexNumber As Integer
-    Public Property Played As Boolean
+    Public Property PlayedTimes As Integer
 End Class
